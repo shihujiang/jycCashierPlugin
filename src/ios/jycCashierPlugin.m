fc3824b44ce1ca52fd5533535053c4b79127abc0
shihujiang <jiangshihu@hotmail.com>
2017-02-22 17:09:37 +0800

    init

---
+++ src/ios/jycCashierPlugin.m
@@ -0,0 +1,28 @@
+/********* jycCashierPlugin.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface jycCashierPlugin : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation jycCashierPlugin
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
